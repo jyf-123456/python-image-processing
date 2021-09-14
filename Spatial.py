5c68ac866d5ef72d92c06eaf9a5c106a5c4730db
Zhonghua Yi <yizhonghua6@gmail.com>
2021-09-15 00:31:27 +0800

    feat(*): 12:31AM, 9/15/2021.

--- Spatial.py
+++ Spatial.py
@@ -1,2 +1,35 @@
+"""
+    Functions from this file are all about image processing in spatial domain.
+"""
+
 import numpy as np
-import cv2 
+
+
+# in_pic is in rgb domain, output is in spatial domain.
+def rgb2gray(in_pic):
+    out = in_pic[:, :, 0] * 0.114 + in_pic[:, :, 1] * 0.587 + in_pic[:, :, 2] * 0.229
+    out = out.astype(np.uint8)
+    return out
+
+
+# get the negative of the in_pic.
+def spatial_reverse(in_pic):
+    out = 255 - in_pic
+    return out
+
+
+def log_transform(in_pic):
+    from math import log
+    c = 255 / log(256)
+    # use 1e-5 to avoid warning.
+    out = c * np.log(1 + in_pic + 1e-5)
+    out = out.astype(np.uint8)
+    return out
+
+
+def gamma_transform(in_pic, gamma):
+    gamma = float(gamma)
+    c = 255 / pow(255, gamma)
+    out = c * np.power(in_pic, gamma)
+    out = out.astype(np.uint8)
+    return out
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import Spatial
+import cv2 as cv
+import numpy as np
+
+IMG_PATH = "azur2021527224121.png"
+
+img = cv.imread(IMG_PATH, 0)
+out = Spatial.gamma_transform(img, 2)
+cv.imshow('img', out)
+cv.waitKey(0)
 
