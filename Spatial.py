054dfcf9fe1bfa7ce61cbca00adeb73443e45d33
Zhonghua Yi <yizhonghua6@gmail.com>
2021-09-16 00:30:12 +0800

    feat(*): 12:31AM, 9/16/2021.

--- Spatial.py
+++ Spatial.py
@@ -5,31 +5,58 @@
 import numpy as np
 
 
-# in_pic is in rgb domain, output is in spatial domain.
-def rgb2gray(in_pic):
-    out = in_pic[:, :, 0] * 0.114 + in_pic[:, :, 1] * 0.587 + in_pic[:, :, 2] * 0.229
-    out = out.astype(np.uint8)
-    return out
+class Spatial:
+
+    def __init__(self, img):
+        self.__origin_img__ = img
+        self.__gray_img = self.__rgb2gray()
+        self.__histogram = self.__get_histogram()
+
+        # in_pic is in rgb domain, output is in spatial domain.
+
+    def __rgb2gray(self):
+        in_pic = self.__origin_img__
+        if in_pic.ndim == 2:
+            out = in_pic
+        else:
+            out = in_pic[:, :, 0] * 0.114 + in_pic[:, :, 1] * 0.587 + in_pic[:, :, 2] * 0.229
+            out = out.astype(np.uint8)
+        return out
+
+    def __get_histogram(self):
+        in_pic = self.__gray_img
+        histogram = np.zeros(256)
+        for i in in_pic.flat:
+            histogram[i] += 1
+        return histogram
+
+    def gray(self):
+        return self.__gray_img
+
+    def histogram(self):
+        return self.__histogram
+
+    # get the negative of the in_pic.
+    def spatial_reverse(self):
+        in_pic = self.__origin_img__
+        out = 255 - in_pic
+        return out
+
+    def log_transform(self):
+        in_pic = self.__origin_img__
+        from math import log
+        c = 255 / log(256)
+        # use 1e-5 to avoid warning.
+        out = c * np.log(1 + in_pic + 1e-5)
+        out = out.astype(np.uint8)
+        return out
+
+    def gamma_transform(self, gamma):
+        in_pic = self.__origin_img__
+        gamma = float(gamma)
+        c = 255 / pow(255, gamma)
+        out = c * np.power(in_pic, gamma)
+        out = out.astype(np.uint8)
+        return out
 
 
-# get the negative of the in_pic.
-def spatial_reverse(in_pic):
-    out = 255 - in_pic
-    return out
-
-
-def log_transform(in_pic):
-    from math import log
-    c = 255 / log(256)
-    # use 1e-5 to avoid warning.
-    out = c * np.log(1 + in_pic + 1e-5)
-    out = out.astype(np.uint8)
-    return out
-
-
-def gamma_transform(in_pic, gamma):
-    gamma = float(gamma)
-    c = 255 / pow(255, gamma)
-    out = c * np.power(in_pic, gamma)
-    out = out.astype(np.uint8)
-    return out
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-import Spatial
+from Spatial import Spatial
 import cv2 as cv
 import numpy as np
+from matplotlib import pyplot as plt
 
 IMG_PATH = "azur2021527224121.png"
 
 img = cv.imread(IMG_PATH, 0)
-out = Spatial.gamma_transform(img, 2)
-cv.imshow('img', out)
-cv.waitKey(0)
+out = Spatial(img)
+histogram = out.histogram()
+x = np.arange(256)
+plt.title("1")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.plot(x, histogram)
+plt.show()
+# cv.imshow('img', out.gray())
+# cv.waitKey(0)
 
